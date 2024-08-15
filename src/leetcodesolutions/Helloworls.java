34250f8d4f5bbdd280ae7de9b7f166a2a5cbf584
Siddardha-CH <siddhusiddhu1712@gmail.com>
2024-08-15 12:32:57 +0530

    Initial commit

---
+++ src/leetcodesolutions/Helloworls.java
@@ -0,0 +1,11 @@
+package leetcodesolutions;
+
+public class Helloworls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module leetcodesolutions {
+}
