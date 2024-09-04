fc04ebfafc7b28052014b3d1293a7b4c0ebbd9c6
Siddardha_CH <siddhusiddhu1712@gmail.com>
2024-09-04 19:58:44 +0530

    Added constrains in comments

--- src/leetcodesolutions/Count_Square_Sum_Triples.java
+++ src/leetcodesolutions/Count_Square_Sum_Triples.java
@@ -4,6 +4,9 @@ public class Count_Square_Sum_Triples {
 //	A square triple (a,b,c) is a triple where a, b, and c are integers and a2 + b2 = c2.
 //
 //			Given an integer n, return the number of square triples such that 1 <= a, b, c <= n.
+
+// Constraints:
+// 1 <= n <= 250
 	class Solution {
 	    public int countTriples(int n) {
 	        int count=0;
