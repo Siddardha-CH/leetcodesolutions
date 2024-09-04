5bf437abba353942baade6532fc62ac4e8df78ea
Siddardha-CH <siddhusiddhu1712@gmail.com>
2024-09-04 19:46:52 +0530

    Count_Special_Quadruplets

---
+++ src/leetcodesolutions/Count_Special_Quadruplets.java
@@ -0,0 +1,27 @@
+package leetcodesolutions;
+
+public class Count_Special_Quadruplets {
+//	Given a 0-indexed integer array nums, return the number of distinct quadruplets (a, b, c, d) such that:
+//
+//		nums[a] + nums[b] + nums[c] == nums[d], and
+//		a < b < c < d
+	class Solution {
+	    public int countQuadruplets(int[] nums) {
+	        int n=nums.length;
+	        int count=0;
+	        for(int a=0;a<n-3;a++){
+	            for(int b=a+1;b<n-2;b++){
+	                for(int c=b+1;c<n-1;c++){
+	                    for(int d=c+1;d<n;d++){
+	                        if(nums[a]+nums[b]+nums[c]==nums[d])
+	                        count++;
+	                    }
+	                }
+	            }
+	        }
+	        return count;
+	        
+	    }
+	}
+
+}
