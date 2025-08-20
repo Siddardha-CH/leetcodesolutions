4b0eb2938ea285492867a87d1ad71a0b85532105
Siddardha_CH <siddhusiddhu1712@gmail.com>
2025-08-20 19:49:12 +0530

    Create 2325DecodetheMessage.py

---
+++ src/leetcodesolutions/2325DecodetheMessage.py
@@ -0,0 +1,29 @@
+# You are given the strings key and message, which represent a cipher key and a secret message, respectively. The steps to decode message are as follows:
+
+# Use the first appearance of all 26 lowercase English letters in key as the order of the substitution table.
+# Align the substitution table with the regular English alphabet.
+# Each letter in message is then substituted using the table.
+# Spaces ' ' are transformed to themselves.
+# For example, given key = "happy boy" (actual key would have at least one instance of each letter in the alphabet), we have the partial substitution 
+# table of ('h' -> 'a', 'a' -> 'b', 'p' -> 'c', 'y' -> 'd', 'b' -> 'e', 'o' -> 'f').
+# Return the decoded message.
+
+------------------------------------------------------------------------------
+
+
+class Solution:
+    def decodeMessage(self, key: str, message: str) -> str:
+        l = []
+        for i in key:
+            if i != ' ' and i not in l:
+                l.append(i)
+        ans = ""
+        for i in message:
+            if i != ' ':
+                ans += chr(l.index(i) + 97)
+            else:
+                ans += ' '
+        return ans
+
+
+        
