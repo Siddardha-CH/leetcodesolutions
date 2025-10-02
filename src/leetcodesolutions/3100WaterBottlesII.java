6cb7452aaa7c32d1aa9ccc0618fcd104149c3e27
Siddardha_CH <siddhusiddhu1712@gmail.com>
2025-10-02 20:12:49 +0530

    Create 3100WaterBottlesII.java

---
+++ src/leetcodesolutions/3100WaterBottlesII.java
@@ -0,0 +1,31 @@
+// You are given two integers numBottles and numExchange.
+
+// numBottles represents the number of full water bottles that you initially have. 
+// In one operation, you can perform one of the following operations:
+
+// Drink any number of full water bottles turning them into empty bottles.
+// Exchange numExchange empty bottles with one full water bottle. Then, increase numExchange by one.
+// Note that you cannot exchange multiple batches of empty bottles for the same value of numExchange. 
+// For example, if numBottles == 3 and numExchange == 1, you cannot exchange 3 empty water bottles for 3 full bottles.
+
+// Return the maximum number of water bottles you can drink.
+
+
+---------------------------------------------------------------------
+
+
+
+  class Solution {
+    public int maxBottlesDrunk(int numBottles, int numExchange) {
+        int  k = numBottles;
+        int n = numExchange;
+        int ans = 0;
+        while (k >= n) {
+            ans += 1;
+            k -= n;
+            n += 1;
+            k += 1;
+        }
+        return ans + numBottles;
+    }
+}
