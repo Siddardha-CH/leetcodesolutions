0e253d85e74b6278d64ca81f8c427184313aa682
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-04-13 20:17:48 +0530

    Create 1848. Minimum Distance to the Target Element.java

---
+++ src/leetcodesolutions/1848. Minimum Distance to the Target Element.java
@@ -0,0 +1,30 @@
+Given an integer array nums (0-indexed) and two integers target and start, find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
+
+Return abs(i - start).
+
+It is guaranteed that target exists in nums.
+
+
+
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+         /*
+        #########################################################################
+        #                                                                       #
+        #  =============================================                        #
+        #                  SIDDARDHA CHILUVERU                                  #
+        #  =============================================                        #
+        #                                                                       #
+        #  Author      : Siddardha Chiluveru                                    #
+        #  Description : Solution / Code / Project                              #
+        #  Date        : 2026-04-13                                             #
+        #                                                                       #
+        #########################################################################
+        */
+        int ans = 9999999;
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] == target)
+                ans = Math.min(ans, Math.abs(i - start));
+        return ans;
+    }
+}
