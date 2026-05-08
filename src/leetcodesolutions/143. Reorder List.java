1de132903096b0933c6db7cccc6a72daaee9df11
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-05-08 18:59:50 +0530

    Create 143. Reorder List.java

---
+++ src/leetcodesolutions/143. Reorder List.java
@@ -0,0 +1,68 @@
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+        /*
+        #########################################################################
+        #                                                                       #
+        #  =============================================                        #
+        #                  SIDDARDHA CHILUVERU                                  #
+        #  =============================================                        #
+        #                                                                       #
+        #  Author      : Siddardha Chiluveru                                    #
+        #  Description : Solution / Code / Project                              #
+        #  Date        : 2026-05-08                                             #
+        #                                                                       #
+        #########################################################################
+        */
+    public void reorderList(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode curr = slow.next;
+        slow.next = null;       // breaking the chain splitiing into two parts
+        ListNode prev = null;
+
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        ListNode og = head;
+        ListNode alt = prev;
+
+        while (alt != null) {
+            ListNode t1 = og.next;
+            ListNode t2 = alt.next;
+            og.next = alt;
+            alt.next = t1;
+            og = t1;
+            alt = t2;
+        }
+    }
+}
