34a21badd8d1253c1dcdef383fb537578cd9bc3d
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-05-12 18:15:54 +0530

    Create 124. Binary Tree Maximum Path Sum.java

---
+++ src/leetcodesolutions/124. Binary Tree Maximum Path Sum.java
@@ -0,0 +1,57 @@
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+The path sum of a path is the sum of the node's values in the path.
+
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+        /*
+        #########################################################################
+        #                                                                       #
+        #  =============================================                        #
+        #                  SIDDARDHA CHILUVERU                                  #
+        #  =============================================                        #
+        #                                                                       #
+        #  Author      : Siddardha Chiluveru                                    #
+        #  Description : Solution / Code / Project                              #
+        #  Date        : 2026-05-12                                             #
+        #                                                                       #
+        #########################################################################
+        */
+        int ans = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    public int dfs(TreeNode root) {
+        if (root == null)
+            return 0;
+        
+        int l = Math.max(0, dfs(root.left));
+        int r = Math.max(0, dfs(root.right));
+
+        ans = Math.max(ans, root.val + l + r);
+
+        return root.val + Math.max(l, r);
+    } 
+}
