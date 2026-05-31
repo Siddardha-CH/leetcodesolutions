d349f1bfc4fee8f1d453f8543769c56dbe3d5425
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-05-31 18:12:22 +0530

    Create 2126. Destroying Asteroids.java

---
+++ src/leetcodesolutions/2126. Destroying Asteroids.java
@@ -0,0 +1,34 @@
+You are given an integer mass, which represents the original mass of a planet. You are further given an integer array asteroids, where asteroids[i] is the mass of the ith asteroid.
+
+You can arrange for the planet to collide with the asteroids in any arbitrary order. If the mass of the planet is greater than or equal to the mass of the asteroid, the asteroid is destroyed and the planet gains the mass of the asteroid. Otherwise, the planet is destroyed.
+
+Return true if all asteroids can be destroyed. Otherwise, return false.
+
+
+
+
+class Solution {
+        /*
+        #########################################################################
+        #                                                                       #
+        #  =============================================                        #
+        #                  SIDDARDHA CHILUVERU                                  #
+        #  =============================================                        #
+        #                                                                       #
+        #  Author      : Siddardha Chiluveru                                    #
+        #  Description : Solution / Code / Project                              #
+        #  Date        : 2026-05-31                                             #
+        #                                                                       #
+        #########################################################################
+        */
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        long m = (long) mass;
+        Arrays.sort(asteroids);
+        for (int i : asteroids) {
+            if (i > m)
+                return false;
+            m += (long) i;
+        }
+        return true;
+    }
+}
