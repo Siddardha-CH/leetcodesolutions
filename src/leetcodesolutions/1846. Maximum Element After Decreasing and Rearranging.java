0d2183e27a7a38d56a1ddac0c35024dd7b62436c
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-06-28 22:13:45 +0530

    Create 1846. Maximum Element After Decreasing and Rearranging.java

---
+++ src/leetcodesolutions/1846. Maximum Element After Decreasing and Rearranging.java
@@ -0,0 +1,37 @@
+You are given an array of positive integers arr. Perform some operations (possibly none) on arr so that it satisfies these conditions:
+
+The value of the first element in arr must be 1.
+The absolute difference between any 2 adjacent elements must be less than or equal to 1. In other words, abs(arr[i] - arr[i - 1]) <= 1 for each i where 1 <= i < arr.length (0-indexed). abs(x) is the absolute value of x.
+There are 2 types of operations that you can perform any number of times:
+
+Decrease the value of any element of arr to a smaller positive integer.
+Rearrange the elements of arr to be in any order.
+Return the maximum possible value of an element in arr after performing the operations to satisfy the conditions.
+
+
+
+
+
+class Solution {
+        /*
+        #########################################################################
+        #                                                                       #
+        #  =============================================                        #
+        #                  SIDDARDHA CHILUVERU                                  #
+        #  =============================================                        #
+        #                                                                       #
+        #  Author      : Siddardha Chiluveru                                    #
+        #  Description : Solution / Code / Project                              #
+        #  Date        : 2026-06-28                                             #
+        #                                                                       #
+        #########################################################################
+        */
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        int c = 1;
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i] >= c + 1)
+                c += 1;
+        return c;
+    }
+}
