b2ae70fdf073a22ab045c413a3b2107d810121a8
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-09-17 13:55:43 +0530

    Create 703. Kth Largest Element in a Stream.java

---
+++ src/leetcodesolutions/703. Kth Largest Element in a Stream.java
@@ -0,0 +1,49 @@
+You are part of a university admissions office and need to keep track of the kth highest test score from applicants in real-time. This helps to determine cut-off marks for interviews and admissions dynamically as new applicants submit their scores.
+
+You are tasked to implement a class which, for a given integer k, maintains a stream of test scores and continuously returns the kth highest test score after a new score has been submitted. More specifically, we are looking for the kth highest score in the sorted list of all scores.
+
+Implement the KthLargest class:
+
+KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of test scores nums.
+int add(int val) Adds a new test score val to the stream and returns the element representing the kth largest element in the pool of test scores so far.
+
+
+
+
+
+class KthLargest {
+        /*
+        #########################################################################
+        #                                                                       #
+        #  =============================================                        #
+        #                  SIDDARDHA CHILUVERU                                  #
+        #  =============================================                        #
+        #                                                                       #
+        #  Author      : Siddardha Chiluveru                                    #
+        #  Description : Solution / Code / Project                              #
+        #  Date        : 2026-17-08                                             #
+        #                                                                       #
+        #########################################################################
+        */
+    PriorityQueue<Integer> pq;
+    int kk;
+    public KthLargest(int k, int[] nums) {
+        kk = k;
+        pq = new PriorityQueue<>();
+        for (int i : nums)
+            pq.offer(i);
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        while (pq.size()!= kk)
+            pq.poll();
+        return pq.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
