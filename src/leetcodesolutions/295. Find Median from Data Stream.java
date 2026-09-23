d65d4018091b62002a87603d6744ecc4d6232da6
Siddardha_CH <siddhusiddhu1712@gmail.com>
2026-09-23 15:15:05 +0530

    Create 295. Find Median from Data Stream.java

---
+++ src/leetcodesolutions/295. Find Median from Data Stream.java
@@ -0,0 +1,61 @@
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted
+
+
+
+
+
+class MedianFinder {
+        /*
+        #########################################################################
+        #                                                                       #
+        #  =============================================                        #
+        #                  SIDDARDHA CHILUVERU                                  #
+        #  =============================================                        #
+        #                                                                       #
+        #  Author      : Siddardha Chiluveru                                    #
+        #  Description : Solution / Code / Project                              #
+        #  Date        : 2026-23-08                                             #
+        #                                                                       #
+        #########################################################################
+        */
+    PriorityQueue<Integer> min;
+    PriorityQueue<Integer> max;
+    public MedianFinder() {
+        min = new PriorityQueue<>();
+        max = new PriorityQueue<>(Collections.reverseOrder());
+    }
+    
+    public void addNum(int num) {
+        if (max.isEmpty() || max.peek() >= num)
+            max.offer(num);
+        else
+            min.offer(num);
+        
+        if (min.size() > max.size())
+            max.offer(min.poll());
+        else if (max.size() > min.size() + 1)
+            min.offer(max.poll());
+    }
+    
+    public double findMedian() {
+        if (max.size() > min.size())
+            return max.peek();
+        return (max.peek() + min.peek()) / 2.0;
+    }
+}
+
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
